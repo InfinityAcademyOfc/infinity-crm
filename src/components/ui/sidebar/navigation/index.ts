69f7fe9482b78442ae2356d8b0a42bac99d0df89
InfinityAcademyOfc <infinityacademyofc@gmail.com>
2025-04-24 19:59:42 -0300

    Delete src directory

--- src/components/ui/sidebar/navigation/index.ts
+++
@@ -1,7 +0,0 @@
-
-export { SidebarMenu } from "./sidebar-menu"
-export { SidebarMenuItem } from "./sidebar-menu-item"
-export { SidebarMenuButton } from "./sidebar-menu-button"
-export { SidebarGroup } from "./sidebar-group"
-export { SidebarGroupLabel } from "./sidebar-group-label"
-export { SidebarGroupContent } from "./sidebar-group-content"
--- src/pages/WhatsAppIntegration.tsx
+++
@@ -1,8 +0,0 @@
-
-import WhatsAppIntegration from "../components/whatsapp/WhatsAppIntegration";
-
-const WhatsAppIntegrationPage = () => {
-  return <WhatsAppIntegration />;
-};
-
-export default WhatsAppIntegrationPage;
