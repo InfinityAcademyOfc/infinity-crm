7807b4cc3627d97bdca2e5b75f0a3a25e2c4d4d2
InfinityAcademyOfc <infinityacademyofc@gmail.com>
2025-05-03 00:13:00 -0300

    Update QRCodeModal.tsx

--- src/components/whatsapp/QRCodeModal.tsx
+++ src/components/whatsapp/QRCodeModal.tsx
@@ -5,16 +5,17 @@ interface QRCodeModalProps {
   open: boolean;
   onOpenChange: (open: boolean) => void;
   sessionId: string;
+  onLogin?: () => void;
 }
 
-const QRCodeModal = ({ open, onOpenChange, sessionId }: QRCodeModalProps) => {
+const QRCodeModal = ({ open, onOpenChange, sessionId, onLogin }: QRCodeModalProps) => {
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="max-w-md">
         <DialogHeader>
           <DialogTitle>Conectar número: {sessionId}</DialogTitle>
         </DialogHeader>
-        <QRCodeScanner sessionId={sessionId} />
+        <QRCodeScanner sessionId={sessionId} onLogin={onLogin} />
       </DialogContent>
     </Dialog>
   );
